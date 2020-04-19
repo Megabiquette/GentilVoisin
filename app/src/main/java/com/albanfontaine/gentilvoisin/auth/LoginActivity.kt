6758c92bc2b5301fcac186dd30c9c5fba79be16f
Megabiquette <megabiquette@gmail.com>
2020-04-19 20:06:50 +0200

    Add MainActivity and nav_graph

--- app/src/main/java/com/albanfontaine/gentilvoisin/auth/LoginActivity.kt
+++ app/src/main/java/com/albanfontaine/gentilvoisin/auth/LoginActivity.kt
@@ -1,19 +1,25 @@
 package com.albanfontaine.gentilvoisin.auth
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.Toast
+import com.albanfontaine.gentilvoisin.MainActivity
 import com.albanfontaine.gentilvoisin.R
 import com.albanfontaine.gentilvoisin.helper.Constants
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.ErrorCodes
+import com.firebase.ui.auth.IdpResponse
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var connectEmailButton: Button
     private lateinit var connectFacebookButton: Button
     private lateinit var connectGoogleButton: Button
-    private lateinit var registerModeButton: Button
+    private lateinit var registerModeButton: Button // ?
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,8 +27,36 @@ class LoginActivity : AppCompatActivity() {
         configurateButtons()
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val response: IdpResponse? = IdpResponse.fromResultIntent(data)
+        if (requestCode == Constants.RC_SIGN_IN) {
+            if (resultCode == Activity.RESULT_OK) {
+                // Go to MainActivity
+                startActivity(Intent(this, MainActivity::class.java))
+            } else {
+                if (response?.error?.errorCode == ErrorCodes.NO_NETWORK) {
+                    Toast.makeText(
+                        this,
+                        resources.getString(R.string.login_error_no_internet),
+                        Toast.LENGTH_LONG
+                    ).show()
+                } else if (response?.error?.errorCode == ErrorCodes.UNKNOWN_ERROR) {
+                    Toast.makeText(
+                        this,
+                        resources.getString(R.string.login_error_unknown),
+                        Toast.LENGTH_LONG
+                    ).show()
+                } else {
+                    return
+                }
+            }
+        }
+    }
+
     private fun enterRegisterMode() {
         Log.e("connect", "reg")
+        // TODO
     }
 
     private fun connect(idpConfigBuilder: AuthUI.IdpConfig.Builder) {
