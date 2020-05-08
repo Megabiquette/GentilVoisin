4778ed7200bd3bdf75b1c950a90176a2a19efde0
Megabiquette <megabiquette@gmail.com>
2020-05-09 01:44:39 +0200

    Add job Adapter for RecyclerView

--- app/src/main/java/com/albanfontaine/gentilvoisin/view/JobAdapter.kt
+++ app/src/main/java/com/albanfontaine/gentilvoisin/view/JobAdapter.kt
@@ -1,4 +1,26 @@
 package com.albanfontaine.gentilvoisin.view
 
-class JobAdapter: Adapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.albanfontaine.gentilvoisin.R
+import com.albanfontaine.gentilvoisin.model.Job
+
+class JobAdapter(private val jobList: List<Job>, val context: Context): RecyclerView.Adapter<JobViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JobViewHolder {
+        val inflater: LayoutInflater = LayoutInflater.from(parent.context)
+        val view: View = inflater.inflate(R.layout.item_jobs_recycler_view, parent, false)
+        return JobViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return jobList.size
+    }
+
+    override fun onBindViewHolder(holder: JobViewHolder, position: Int) {
+        holder.updateWithJob(jobList[position], context)
+    }
 }
