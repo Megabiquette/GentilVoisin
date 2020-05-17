cbc60cd72881c4dd8846dbc7ec7ba17d9c934a93
Megabiquette <megabiquette@gmail.com>
2020-05-17 20:36:06 +0200

    Finished AddJobFragment layout

--- app/src/main/java/com/albanfontaine/gentilvoisin/core/AddJobFragment.kt
+++ app/src/main/java/com/albanfontaine/gentilvoisin/core/AddJobFragment.kt
@@ -1,20 +1,24 @@
 package com.albanfontaine.gentilvoisin.core
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.RadioButton
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
 
 import com.albanfontaine.gentilvoisin.R
 import com.albanfontaine.gentilvoisin.model.User
 import com.albanfontaine.gentilvoisin.repository.UserRepository
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_add_job.*
 
 class AddJobFragment : Fragment() {
     private lateinit var userCity: String
+    private lateinit var categoriesSpinner: Spinner
+    private lateinit var submitButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,7 +34,30 @@ class AddJobFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add_job, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        configureViews()
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    private fun configureViews() {
+        categoriesSpinner = add_job_spinner_category
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.job_categories,
+            R.layout.item_spinner_custom
+        )
+        .also { adapter ->
+            adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item)
+            categoriesSpinner.adapter = adapter
+        }
+
+        submitButton = add_job_button.apply {
+            setOnClickListener {
+
+            }
+        }
     }
 }
