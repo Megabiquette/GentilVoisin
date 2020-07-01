cb4ef90648abdd7bfcba3ca02d04074700767a8e
Megabiquette <megabiquette@gmail.com>
2020-07-02 01:24:08 +0200

    Start work on RatingsActivity

---
+++ app/src/main/java/com/albanfontaine/gentilvoisin/user/RatingsActivity.kt
@@ -0,0 +1,12 @@
+package com.albanfontaine.gentilvoisin.user
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.albanfontaine.gentilvoisin.R
+
+class RatingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ratings)
+    }
+}
